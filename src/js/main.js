b2fa06f850e73c64dd026908a29bf737e2bfd26d
Pavel <paha_gagarin@mail.ru>
2023-09-07 16:28:18 +0300

    удаление логики попап и  верстка  auth-info

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,12 @@
 import { swiperCustom } from "./swiperCustom";
 import { fixedMenu } from "./fixedMenu";
-// import { fixedSidebar } from "./fixedSidebar";
 import modals from "./modals";
 import tabs from "./tabs";
 import { getRangeSlider } from "./rangeSlider";
-import { toggleDisplayPopup } from "./togglePopup";
 window.addEventListener('DOMContentLoaded', function () {
   swiperCustom();
   fixedMenu();
   modals()
   tabs()
   getRangeSlider()
-  toggleDisplayPopup()
-  // fixedSidebar()
 });
